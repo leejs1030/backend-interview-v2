1002b4dc6423372d56bc96fff437b307f913dc5e
leejs1030 <leejs1030@korea.ac.kr>
2022-03-10 20:58:19 +0900

    Add schema

---
+++ schema/ddl.sql
@@ -0,0 +1,34 @@
+DROP TABLE descriptions, sizes;
+DROP TABLE products, users;
+-- 혹시 테이블이 이미 있었다면 미리 초기화 하기
+
+CREATE TABLE users(
+    id VARCHAR(20),
+    password VARCHAR(200) NOT NULL,
+    nickname VARCHAR(20) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE products(
+    id SERIAL,
+    name VARCHAR(50) NOT NULL,
+    brand VARCHAR(200),
+    price INTEGER,
+    color VARCHAR(30),
+    likes INTEGER DEFAULT 0,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE sizes(
+    id INTEGER,
+    size VARCHAR(7),
+    FOREIGN KEY (id) REFERENCES products(id) ON DELETE CASCADE,
+    PRIMARY KEY(id, size)
+);
+
+CREATE TABLE descriptions(
+    id INTEGER,
+    content VARCHAR(10000),
+    primary key(id),
+    FOREIGN KEY (id) REFERENCES products(id) ON DELETE CASCADE
+);
