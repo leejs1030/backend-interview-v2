1ca1f19994b372f18b540aa4564662dd4c73134b
leejs1030 <leejs1030@korea.ac.kr>
2022-03-12 01:51:30 +0900

    Test: Enhance test code for /products

--- test/products/products.spec.ts
+++ test/products/products.spec.ts
@@ -1,23 +1,27 @@
 import * as chai from "chai";
-import chaiHttp = require('chai-http');
+import { expect } from "chai";
+chai.use(require('chai-http'));
 import 'mocha';
-import db from '../../src/lib/dbconnection';
 import { AppModule } from '../../src/app.module';
 import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 
-chai.use(chaiHttp);
-let chaiLib = <any>chai;
-let chaiRequestLib = chaiLib.default.request;
+import db from '../../src/lib/dbconnection';
+
+// let chaiLib = <any>chai;
+// let chaiRequestLib = chaiLib.default.request;
+let request = (chai as any).default.request;
 
 describe('Test products', () =>{
     let app: INestApplication;
+    let server: any;
 
     beforeEach(async () => {
       const module = await Test.createTestingModule({
         imports: [AppModule],
       }).compile();
       app = module.createNestApplication();
+      server = app.getHttpServer();
       await app.init();
     });
   
@@ -26,6 +30,11 @@ describe('Test products', () =>{
     });
   
     it('products test', async () =>{
-        const ret = await chaiRequestLib(app.getHttpServer()).get('/products');
-    })
+        const ret = await request(server).get('/products');
+        console.log(ret.text);
+        console.log(ret.header);
+        expect(ret.status).equal(200);
+        const obj = JSON.parse(ret.text);
+        expect(obj.data.length).equal((await db.one('SELECT count(*)::int FROM products;')).count);
+    });
 })
