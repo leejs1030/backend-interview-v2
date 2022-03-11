f7c75c96ed3f2d7e1deb13099cec13319ad52658
leejs1030 <leejs1030@korea.ac.kr>
2022-03-12 07:13:00 +0900

    Update auth controller.ts
    - Update to use restful json

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Post, UseGuards, Req, Body, Get } from '@nestjs/common';
+import { Controller, Post, UseGuards, Req, Body, Get, Res, Header, HttpCode } from '@nestjs/common';
 import { user } from 'custom-type';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { LocalAuthGuard } from './local-auth.guard';
@@ -11,23 +11,60 @@ export class AuthController {
     
     @UseGuards(LocalAuthGuard)
     @Post('/sign-in')
-    async login(@Req() req: Request, @Body() body: any) {
-        console.log(req.user);
-        console.log(body);
-
-        return this.authService.login(req.user);
+    @Header('content-type', 'application/hal+json')
+    async login(@Req() req: Request, @Body() body: any, @Res() res: Response) {
+        const ret = await this.authService.login(req.user);
+        res.status(200);
+        const result = {
+            data: ret,
+            _links: [
+                {
+                    rel: "self",
+                    href: "/auth/sign-in",
+                    type: "POST",
+                }
+            ]
+        }
+        res.json(result);
     }
 
     @Post('/sign-up')
-    async signup(@Body() body: user) {
-        return await this.authService.createUser(body);
+    @Header('content-type', 'application/hal+json')
+    async signup(@Body() body: any, @Res() res: Response) {
+        body = { id: body.username, password: body.password, nickname: body.nickname } as user;
+        const ret = await this.authService.createUser(body);
+        if(!ret) return res.status(409).send('ID already exists!');
+        res.status(201);
+        const result = {
+            data: ret,
+            _links:[
+                {
+                    rel: "self",
+                    href: "/auth/sign-up",
+                    type: "POST",
+                }
+            ]
+        }
+        return res.json(result);
     }
 
     @UseGuards(JwtAuthGuard)
     @Get('profile')
+    @HttpCode(200)
+    @Header('content-type', 'application/hal+json')
     getProfile(@Req() req: Request) {
-        console.log('hi');
-        console.log(req.user);
-        return req.user;
+        const ret = req.user;
+        console.log(ret);
+        const result = {
+            data: ret,
+            _links: [
+                {
+                    rel: "self",
+                    href: "/auth/profile",
+                    type: "GET",
+                }
+            ]
+        }
+        return result;
     }
 }
