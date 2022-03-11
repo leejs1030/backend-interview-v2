e12b6a09a5b2fb9a9f1f9d3a3daf3633b1ae3b6b
leejs1030 <64299554+leejs1030@users.noreply.github.com>
2022-03-12 07:42:48 +0900

    Likes (#3)
    * Change indent
    
    * Update indent
    
    * Feat: Added like
    
    * Apply strict and case sensitive routing

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   console.log(process.env.DB_HOST);
   const app = await NestFactory.create(AppModule);
+  const httpAdapter = app.getHttpAdapter().getInstance();
+  httpAdapter.set('strict routing', true);
+  httpAdapter.set('case sensitive routing', true);
   await app.listen(3000);
 }
 bootstrap();
